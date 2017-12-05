4950f04faf7b8f920ddc17c1d0fef6fb21af899a
Placements 2018 <yashchaudhary01212@gmail.com>
2017-12-05 22:48:45 +0530

    Hh

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 resources :trucks
+root 'truck#show_trucks'
 get '/get_fields', to: 'truck#get_fields'
 post '/add_truck', to: 'truck#add_truck'
 get '/show_trucks', to: 'truck#show_trucks'
